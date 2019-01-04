14c508cbc8c0b050ddb0b000969bf86785683554
Jaydip Parmar <parmarjaydip13@gmail.com>
2019-01-04 21:49:55 +0530

    Added Login and Registration on SPA

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
+
+  register() {
+    this.authService.Register(this.model).subscribe(
+      () => {
+        console.log('Register success');
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
